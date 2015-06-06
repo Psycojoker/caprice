59e117dc033500a8aef78a68d51fc87dc0214cad
Laurent Peuch <cortex@worlddomination.be>
2015-06-06 05:38:37 +0200

    [enh] another fail tolerent command

--- caprice/core/shell.py
+++ caprice/core/shell.py
@@ -5,3 +5,8 @@ from caprice.common.core.logging import log
 def command(arguments):
     log._shell(" ".join(arguments))
     return subprocess.check_call(arguments)
+
+
+def command_can_fail(arguments):
+    log._shell(" ".join(arguments))
+    return subprocess.call(arguments)
